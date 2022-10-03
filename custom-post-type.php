ac8280babdeee0d5f7ab81d1bd035ab842b50ebe
Vasyl Sokolyk <vsokolyk@gmail.com>
2022-10-03 19:08:14 +0300

    refactor(core): set this plugin as muplugin

---
+++ custom-post-type.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:         custom-post-type
+ * Plugin URI:          https://github.com/Jazz-Man/custom-post-type
+ * Description:         A single class to help you build more advanced custom post types quickly.
+ * Author:              Vasyl Sokolyk
+ * Author URI:          https://www.linkedin.com/in/sokolyk-vasyl
+ * Requires at least:   5.2
+ * Requires PHP:        7.4
+ * License:             MIT
+ * Update URI:          https://github.com/Jazz-Man/custom-post-type.
+ */
+
+use JazzMan\Post\ArchivePostType;
+
+if (function_exists('app_autoload_classes')) {
+    app_autoload_classes([
+        ArchivePostType::class,
+    ]);
+}
