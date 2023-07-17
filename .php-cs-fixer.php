5b8deb0a211b324aceaa28fed542dc8071460838
Vasyl Sokolyk <vsokolyk@gmail.com>
2023-07-17 14:10:48 +0300

    build(core): disabled cache dir

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,7 +2,6 @@
 
 use function JazzMan\PhpCsFixerRules\phpCsFixerConfig;
 
-require_once __DIR__ . '/vendor/jazzman/php-cs-fixer-rules/src/rules-config.php';
+require_once __DIR__.'/vendor/jazzman/php-cs-fixer-rules/src/rules-config.php';
 
-
-return phpCsFixerConfig(__DIR__);
+return phpCsFixerConfig( __DIR__ );
